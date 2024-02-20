57319e87f64234ff73b7a9d1db3653379b3089d2
wuzhuzhu <walterr@live.cn>
2024-02-20 18:56:19 +0800

    [feat]github oauth ok

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+"use client";
+import { DEFAULT_LOGIN_REDIRECT } from "@/lib/constants/path";
+import { signIn } from "next-auth/react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 import { Button } from "../ui/button";
 
 const Social = () => {
+	const handleLogin = async (provider: string) => {
+		await signIn(provider, { callbackUrl: DEFAULT_LOGIN_REDIRECT });
+	};
 	return (
 		<div className="flex items-center w-full gap-2">
-			<Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+			<Button
+				onClickCapture={() => handleLogin("github")}
+				size="lg"
+				className="w-full"
+				variant="outline"
+				onClick={() => {}}
+			>
 				<FaGithub size={18} />
 			</Button>
-			<Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+			<Button
+				onClickCapture={() => handleLogin("google")}
+				size="lg"
+				className="w-full"
+				variant="outline"
+				onClick={() => {}}
+			>
 				<FaGoogle size={18} />
 			</Button>
 		</div>
--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -40,9 +40,12 @@ export default {
 			},
 		}),
 		GitHub({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		}),
+		Google({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
-		Google,
 	],
 } satisfies NextAuthConfig;
