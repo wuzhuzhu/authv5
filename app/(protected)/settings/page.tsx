4d1f015d6a5db7015b34d03b94d0e8ada7688441
wuzhuzhu <walterr@live.cn>
2024-02-20 16:16:41 +0800

    [feat]sign out use server action

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
 import { auth } from "@/lib/auth";
+import { signOut } from "@/lib/auth";
 
 const SettingPage = async () => {
 	const session = await auth();
 	return (
 		<div>
+			<form
+				action={async () => {
+					"use server";
+					await signOut();
+				}}
+			>
+				<Button>Sign Out</Button>
+			</form>
 			SettingPage
 			<p>{JSON.stringify(session)}</p>
 		</div>
