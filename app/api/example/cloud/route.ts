1d7a113ee9ff690472566da5c8ed7b14e8c77162
wuzhuzhu <walterr@live.cn>
2024-03-04 12:53:43 +0800

    [feat]data fetching with loading example [dev]plugin suggestion, introducing date-fn

--- app/api/example/cloud/route.ts
+++ app/api/example/cloud/route.ts
@@ -1,3 +1,4 @@
+import { sleep } from "@/lib/utils";
 import { headers } from "next/headers";
 // https://nextjs.org/docs/app/building-your-application/routing/route-handlers
 // https://github.com/nextauthjs/next-auth/issues/7423
@@ -5,6 +6,7 @@ import { headers } from "next/headers";
 // export const dynamic = "force-dynamic"; // defaults to auto
 
 export async function GET(request: Request) {
+	await sleep(1000);
 	// No login required example, don't use headers
 	const res = await fetch(
 		"https://cloud.siliconflow.com/api/v1/playground/cloud",
@@ -13,5 +15,5 @@ export async function GET(request: Request) {
 		},
 	);
 	const data = await res.json();
-	return Response.json({ data });
+	return Response.json(data);
 }
--- lib/data/example.ts
+++ lib/data/example.ts
@@ -1,9 +1,15 @@
 import { sleep } from "@/lib/utils";
+import { format } from "date-fns";
 
 export async function getClouds() {
 	await sleep(1000);
 	const res = await fetch(
 		"https://cloud.siliconflow.com/api/v1/playground/cloud",
+		{
+			next: {
+				revalidate: 600, // revalidate at most every 10 minutes
+			},
+		},
 	);
 	// The return value is *not* serialized
 	// You can return Date, Map, Set, etc.
@@ -18,5 +24,23 @@ export async function getClouds() {
 
 export async function getError() {
 	await sleep(1000);
-	throw new Error("A error should be throwed.");
+	const fiftyPercent = Math.random() > 0.5;
+	if (fiftyPercent) throw new Error("a error happend");
+	const res = await fetch(
+		"https://cloud.siliconflow.com/api/v1/playground/cloud",
+		{
+			next: {
+				revalidate: 600, // revalidate at most every 10 minutes
+			},
+		},
+	);
+	// The return value is *not* serialized
+	// You can return Date, Map, Set, etc.
+
+	if (!res.ok) {
+		// This will activate the closest `error.js` Error Boundary
+		throw new Error("Failed to fetch data");
+	}
+
+	return res.json();
 }
