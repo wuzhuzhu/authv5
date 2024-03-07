39ca16fc32752c416319cc2cf2722fc03670595e
wuzhuzhu <walterr@live.cn>
2024-03-07 22:08:24 +0800

    [feat]minor change

--- app/(protected)/example/form/components/form.tsx
+++ app/(protected)/example/form/components/form.tsx
@@ -8,48 +8,58 @@ import validator from "@rjsf/validator-ajv8";
 import { JSONSchema7 } from "json-schema";
 
 const testSchema: JSONSchema7 = {
-  title: "表单标题内容",
-  description: "简介内容,一些简单的介绍.",
-  type: "object",
-  required: ["name"],
-  properties: {
-    name: {
-      type: "string",
-      title: "姓名",
-      default: "Ch",
-      minLength: 3,
-      // description: "请输入你的姓名",
-    },
-    age: {
-      type: "number",
-      title: "年龄",
-      default: 35,
-      minimum: 18,
-      // description: "请输入你的年龄",
-    },
-  },
+	title: "表单标题内容",
+	description: "简介内容,一些简单的介绍.",
+	type: "object",
+	required: ["name"],
+	properties: {
+		name: {
+			type: "string",
+			title: "姓名",
+			default: "Ch",
+			minLength: 3,
+			// description: "请输入你的姓名",
+		},
+		age: {
+			type: "number",
+			title: "年龄",
+			default: 35,
+			minimum: 18,
+			// description: "请输入你的年龄",
+		},
+	},
 };
 
 const testUiSchema = {
-  "ui:submitButtonOptions": {
-    submitText: "提交",
-  },
+	"ui:submitButtonOptions": {
+		submitText: "提交",
+	},
 };
 
 const DynamicForm = () => {
-  return (
-    <div className="schema-form px-8 py-4 bg-muted">
-      <Form
-        schema={exampleFormSchema}
-        uiSchema={exampleFormUiSchema}
-        noHtml5Validate
-        validator={validator}
-        onChange={(e) => console.log("change", e)}
-        onSubmit={(e) => console.log("submit", e)}
-        onError={(e) => console.log("error", e)}
-      />
-    </div>
-  );
+	const formRef = useRef(null);
+	return (
+		<div className="schema-form px-8 py-4 bg-muted">
+			<Form
+				ref={formRef}
+				schema={exampleFormSchema}
+				uiSchema={exampleFormUiSchema}
+				noHtml5Validate
+				validator={validator}
+				onChange={(e) => console.log("change", e)}
+				onSubmit={(e) =>
+					console.log("submit", {
+						...e,
+						formData: {
+							...e.formData,
+							prompt: "123123",
+						},
+					})
+				}
+				onError={(e) => console.log("error", e)}
+			/>
+		</div>
+	);
 };
 
 export default DynamicForm;
--- app/(protected)/models/image/[[...modelId]]/page.tsx
+++ app/(protected)/models/image/[[...modelId]]/page.tsx
@@ -5,12 +5,15 @@ const ImageModelPage = async () => {
 	return (
 		<>
 			<form className="flex gap-2 h-full" action="generate">
-				<div className="flex-1 bg-white rounded-md p-6">
-					<h3>
-						Preview Image Here12312312123123123
-						12312312312123123123123123123 123123 123123123
-					</h3>
-					<p>{JSON.stringify(data?.data)}</p>
+				<div className="flex flex-col justify-between flex-1 bg-white rounded-md p-6">
+					<div>
+						<h3>
+							Preview Image Here12312312123123123
+							12312312312123123123123123123 123123 123123123
+						</h3>
+						<p>{JSON.stringify(data?.data)}</p>
+					</div>
+					<div>prompts</div>
 				</div>
 				<div className="basis-[320px] bg-white p-6 rounded-md">
 					Form
