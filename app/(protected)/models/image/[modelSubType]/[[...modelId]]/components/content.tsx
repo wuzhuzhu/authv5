225a2c8da73519b83f4e96e8ba9cfd832bdfee01
wuzhuzhu <walterr@live.cn>
2024-03-12 12:08:13 +0800

    [style]full page layout and scroll

--- app/(protected)/models/image/[modelSubType]/[[...modelId]]/components/content.tsx
+++ app/(protected)/models/image/[modelSubType]/[[...modelId]]/components/content.tsx
@@ -3,6 +3,8 @@
 import DynamicForm from "@/app/(protected)/example/form/components/form";
 import PromptSuggestions from "@/components/shared/models/prompt-suggestions";
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { useRef } from "react";
 
 const ImagePageContent = ({ children }: { children: React.ReactNode }) => {
@@ -11,15 +13,19 @@ const ImagePageContent = ({ children }: { children: React.ReactNode }) => {
 		<>
 			<div className="flex gap-2 h-full">
 				{/* preview part as a children */}
-				<div className="flex flex-col justify-between flex-1 bg-white rounded-md p-6">
-					{children}
-					<div>
-						<Button onClick={() => formRef.current?.submit()}>
-							Click me 1
+				<div className="flex flex-col gap-8 justify-between flex-1 bg-white rounded-md px-6 py-4 overflow-hidden">
+					<div className="flex-1 overflow-y-scroll">{children}</div>
+					<div className="relative">
+						<Textarea
+							placeholder="Type your prompt here."
+							id="message-2"
+						/>
+						<Button className="absolute right-2 bottom-2" size="sm">
+							Send message
 						</Button>
 					</div>
 				</div>
-				<div className="basis-[320px] bg-white p-6 rounded-md">
+				<div className="basis-[320px] bg-white p-6 rounded-md overflow-y-scroll">
 					<DynamicForm formRef={formRef} />
 				</div>
 			</div>
